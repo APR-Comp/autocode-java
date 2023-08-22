07a26d95c4d1fd8bd870f4e3e8c868370f29fe94
Martin Mirchev <mirchevmartin2203@gmail.com>
2023-08-22 18:09:25 +0800

    Add positive test cases

---
+++ modify-graph-edge-weights/1/src/test/java/com/aprcomp/Test_3.java
@@ -0,0 +1,13 @@
+
+    package com.aprcomp;
+    import java.util.*;
+    import org.junit.Test;
+    import static com.aprcomp.Solution.*;
+    import static org.junit.Assert.*;
+    public class Test_3 {
+        @Test(timeout=5000)
+        public void test_3(){
+            assertArrayEquals(new int[][]{{1,0,4}}, new Solution().modifiedGraphEdges( 2, new int[][]{{1,0,4}}, 1, 0, 4));
+            }
+    }
+    
---
+++ modify-graph-edge-weights/2/src/test/java/com/aprcomp/Test_4.java
@@ -0,0 +1,13 @@
+
+    package com.aprcomp;
+    import java.util.*;
+    import org.junit.Test;
+    import static com.aprcomp.Solution.*;
+    import static org.junit.Assert.*;
+    public class Test_4 {
+        @Test(timeout=5000)
+        public void test_4(){
+            assertArrayEquals(new int[][]{}, new Solution().modifiedGraphEdges( 2, new int[][]{{1,0,4}}, 1, 0, 3));
+            }
+    }
+    
